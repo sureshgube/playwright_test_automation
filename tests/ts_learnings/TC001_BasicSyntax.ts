2c926ac6a65a2703e34b30c349e2024c78eb9d42
surehgube <sureshselvamkrishnan@gmail.com>
2026-08-11 23:46:52 +0530

    basics secnd

--- tests/ts_learnings/TC001_BasicSyntax.ts
+++ tests/ts_learnings/TC001_BasicSyntax.ts
@@ -6,3 +6,24 @@ console.log(message)
 var message1="Hello World11"
 console.log(message1)
 
+//learn variables
+var namee:string = "Alice";
+console.log(namee);
+
+//learn constants
+const pi:number = 3.14;
+console.log(pi);
+
+//learn let
+let age:number = 25;
+console.log(age);
+
+//learn data types
+var isActive:boolean = true;
+console.log(isActive);
+
+var count:number = 10;
+console.log(count);
+
+
+
